3d20a1e0746b2f127e5860b8ac919d9099b43e55
vishal <vishalshetty.k@gmail.com>
2021-11-26 10:50:43 +0000

    changed the Vam name tag

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "server2" {
   ami           = "ami-0ba62214afa52bec7"
   instance_type = "t2.medium"
   tags = {
-    Name = "vishal-terraform-cloudVm"
+    Name = "vishal-terraform-cloudVm2"
     Env  = "Prod"
   }
 }
